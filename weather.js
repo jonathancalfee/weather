4378a266be059dc310005cc473b6708d5ef70e22
jonathancalfee <j-calfee@kellogg.northwestern.edu>
2016-01-25 15:14:22 -0600

    Adding the weather files

---
+++ weather.js
@@ -0,0 +1,27 @@
+$(function() {
+  var handleWeatherResponse = function(data) {
+    console.log(data);
+
+    // Put your code here to change the "markup" variable.
+    // Don't change any other code in this file. You will be sad.
+
+    var markup = "The weather is " +data.currently.temperature;
+    markup=markup + " and the current weather is " +data.currently.summary + ".";
+    markup=markup + "Tomorrow the weather will be " +data.daily.data[1].summary;
+    markup=markup + "The day after will be " +data.daily.data[2].summary;
+    markup=markup + " 3 days from today the weather will be " +data.daily.data[3].summary;
+    // End of your code
+
+    $('.weather-report').html(markup);
+  }
+  $('a.get-the-weather').on('click', function(event) {
+    event.preventDefault();
+    $.ajax({
+      type: 'GET',
+      url: 'https://api.forecast.io/forecast/6dbe98374cc5b8f9ea63d5ec73de9c04/42.056459,-87.675267?callback=?',
+      dataType: 'jsonp',
+      contentType: "application/json",
+      success: handleWeatherResponse
+    });
+  });
+});
